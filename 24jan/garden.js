3705e638f292cbe927df32437a8848871ed577a6
Arpit Kaushal <kaushal.arpit@gmail.com>
2026-02-11 08:30:47 +0530

    change the hues of circle with frame gradually

--- 24jan/garden.js
+++ 24jan/garden.js
@@ -18,14 +18,23 @@ let chosenColor;
 
 function setup() {
     createCanvas(400, 400);
+    colorMode(HSB)
+    background("black");
 }
+let saturation = 20; 
+let brightness = 20;
+let opacity = 5;
 
 function draw() {
-    chosenColor = colorOptions[colorIndex];
-    background(chosenColor.sky);
-    fill(chosenColor.ocean);
-    mouseLocation();
-    rect()
+    // chosenColor = colorOptions[colorIndex];
+    saturation+=10
+    brightness+=2
+    opacity+=((opacity+5)%20)/20;
+    fill(120,saturation%100,brightness%100,opacity);
+    strokeWeight(20)
+    stroke(120,saturation%100,brightness%100,opacity)
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    // mouseLocation();
 }
 
 function mouseLocation() {
