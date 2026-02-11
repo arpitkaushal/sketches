16ca1d5a630422f10400b4ec7b2deb942af66dad
Arpit Kaushal <kaushal.arpit@gmail.com>
2026-02-11 08:40:14 +0530

    sketch boiler plate

---
+++ plaige-isolle-polo-and-pan/sketch.js
@@ -0,0 +1,33 @@
+const color = {
+    "sky": "#AEB5C7",
+    "ocean": "#6894D1",
+    "sand": "#FDD427",
+    "treebark": "#71573D",
+    "treeleaves": "#9CC756"
+}
+const bnw = {
+    "sky": "#AEB5C7",
+    "ocean": "#6894D1",
+    "sand": "#FDD427",
+    "treebark": "#71573D",
+    "treeleaves": "#9CC756"
+}
+
+colorOptions = [color, bnw]
+colorIndex = 0;
+let chosenColor;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    chosenColor = colorOptions[colorIndex];
+    background(chosenColor.sky);
+    mouseLocation();
+}
+
+function mouseLocation() {
+    fill(0)
+    text(`${int(mouseX)},${int(mouseY)}`, 20, 20);
+}
